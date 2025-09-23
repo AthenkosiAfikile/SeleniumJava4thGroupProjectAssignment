4ddca2e58bf24cdc1d98e5ce65d2755d3c81b95a
User <breakathi@gmail.com>
2025-09-23 23:50:17 +0200

    Implemented Learning Screen (Log In)

--- src/test/java/ndosiTest/NdosiTests.java
+++ src/test/java/ndosiTest/NdosiTests.java
@@ -23,11 +23,58 @@ public class NdosiTests extends Base {
 
     @Test(dependsOnMethods = "verifyLoginPageIsDisplayedTests")
     public void enterEmailAddressTests() {
-        loginPage.enterEmailAddress("makes@gmail.com");
+        loginPage.enterEmailAddress("aab@ndosi.com");
     }
 
-    @AfterTest
-    public void closeBrowser() {
-        driver.quit();
+    @Test(dependsOnMethods = "enterEmailAddressTests")
+    public void incorrectPasswordTest() {
+        loginPage.enterPassword("123");
     }
+
+    @Test(dependsOnMethods = "incorrectPasswordTest")
+    public void clickLoginWithInvalidInvalidPasswordTest() {
+        loginPage.clickLoginButton();
+    }
+
+    @Test(dependsOnMethods = "clickLoginWithInvalidInvalidPasswordTest")
+    public void acceptLoginAlertTest() {
+        loginPage.loginFailedAcceptLoginAlert();
+    }
+
+    @Test(dependsOnMethods = "acceptLoginAlertTest")
+    public void clearPasswordTest() {
+        loginPage.clearPassword();
+    }
+
+    @Test(dependsOnMethods = "clearPasswordTest")
+    public void enterPasswordTests() {
+        loginPage.enterPassword("1234567890");
+    }
+
+    @Test(dependsOnMethods = "enterPasswordTests")
+    public void clickLoginButtonTests() {
+        loginPage.clickLoginButton();
+    }
+
+    @Test(dependsOnMethods = "clickLoginButtonTests")
+    public void missingRequiredFieldsAlert() {
+        loginPage.missingRequiredFieldsAlert();
+    }
+
+    @Test(dependsOnMethods = "missingRequiredFieldsAlert")
+    public void enterEmailAddressWithPasswordAfterCleanTest() {
+        enterEmailAddressTests();
+        enterPasswordTests();
+    }
+
+    @Test(dependsOnMethods = "enterEmailAddressWithPasswordAfterCleanTest")
+    public void clickLoginButtonAfterCleanEmailFieldTests() {
+        loginPage.clickLoginButton();
+    }
+
+
+//    @AfterTest
+//    public void closeBrowser() {
+//        driver.quit();
+//    }
 }
--- src/test/java/screens/LoginPage.java
+++ src/test/java/screens/LoginPage.java
@@ -1,9 +1,12 @@
 package screens;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
@@ -18,19 +21,50 @@ public class LoginPage {
     @FindBy(id = "login-email")
     WebElement emailField_id;
 
+    @FindBy(id = "login-password")
+    WebElement passwordField_id;
+
+    @FindBy(id = "login-submit")
+    WebElement loginButton_id;
+
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void verifyLoginPageIsDisplayed() {
-        new WebDriverWait(driver, Duration.ofSeconds(10)).until(visibilityOf(loginPageTitle_id));
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(visibilityOf(loginPageTitle_id));
         loginPageTitle_id.isDisplayed();
     }
 
     public void enterEmailAddress(String email) {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(visibilityOf(loginPageTitle_id));
         emailField_id.sendKeys(email);
 
     }
 
-}
+    public void enterPassword(String password) {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(visibilityOf(loginPageTitle_id));
+        passwordField_id.sendKeys(password);
+    }
 
+    public void clickLoginButton() {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(visibilityOf(loginPageTitle_id));
+        loginButton_id.click();
+    }
+
+    public void clearPassword() {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(visibilityOf(passwordField_id));
+        passwordField_id.clear();
+    }
+
+    public void loginFailedAcceptLoginAlert() {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert().accept();
+    }
+
+    public void missingRequiredFieldsAlert() {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert().accept();
+    }
+
+}
