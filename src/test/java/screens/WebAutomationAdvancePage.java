c4219c53d8c87eb2b552c397a46682bf955f2743
User <breakathi@gmail.com>
2025-09-24 16:24:40 +0200

    Added Web AutomationAdvance Page

--- src/test/java/screens/WebAutomationAdvancePage.java
+++ src/test/java/screens/WebAutomationAdvancePage.java
@@ -1,4 +1,26 @@
 package screens;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class WebAutomationAdvancePage {
+    WebDriver driver;
+
+    @FindBy(id= "tab-btn-web")
+    WebElement webAutomationAdvanceTab_id;
+
+    public WebAutomationAdvancePage(WebDriver driver) {
+        this.driver = driver;
+    }
+    public void verifyWebAutomationAdvancePageIsDisplayed() {
+        new WebDriverWait(driver, Duration.ofSeconds(20)).
+                until(ExpectedConditions.visibilityOf(webAutomationAdvanceTab_id));
+        webAutomationAdvanceTab_id.isDisplayed();
+        webAutomationAdvanceTab_id.click();
+    }
 }
